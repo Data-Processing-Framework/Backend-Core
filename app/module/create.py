49377c731dff79cfa46e2d8809ad9145d2b0ccde
Adaliii <adaliavictor@gmail.com>
2023-03-27 12:57:41 +0200

    module POST done

--- app/module/create.py
+++ app/module/create.py
@@ -1,5 +1,24 @@
 from flask import jsonify
-
+import zmq
 
 def create(request):
-    return jsonify({"status": 200, "errors": ["bad parsing"]})  # Exemple de resposta
+    name = request['name']
+    id = request['id']
+    type = request['type']
+    module = request['module']
+
+    
+
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("ipc://backend.ipc")
+    #socket.send_json({"name": name, "id": id, "type": type, "module": module})
+    socket.send(f'POST;{name};{id};{type};{module}')
+
+    #  Get the reply.
+    message = socket.recv()
+    if message == b'OK':
+        return jsonify({"status": 200})
+    else:
+        return jsonify({"errors": [{"error": "", "message": "", "detail": ""}], "code": 400}) 
+
