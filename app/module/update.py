cb966cc2ade5e8d789353950dfc352918ea4535f
mjordanaam <mjordana17@gmail.com>
2023-03-27 13:15:09 +0200

    Module PUT provisional

--- app/module/update.py
+++ app/module/update.py
@@ -1,5 +1,19 @@
 from flask import jsonify
+import zmq
 
 
 def update(request, moduleId):
-    return jsonify({"status": 200})  # Exemple de resposta
+    name = request['name']
+    type = request['type']
+    module = request['module']
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("ipc://backend.ipc")
+    socket.send(f'{name};{type};{module};PUT;MODULE;{moduleId}')
+
+    message = socket.recv()
+
+    if message == '200':
+        return jsonify({"status": 200})
+    else:
+        return jsonify({"errors": [{"error": "", "message": "", "detail": ""}], "code": 400}) 
