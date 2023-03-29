8e024770d3b0eabfc5e679ecdbc98510025fd8b9
AlSaBib98 <92037047+AlSaBib98@users.noreply.github.com>
2023-03-29 10:51:08 +0200

    Get uploaded
    Get uploaded

--- app/module/get.py
+++ app/module/get.py
@@ -1,5 +1,73 @@
+from module_m import addToList, n_module, getList, createModule
 from flask import jsonify
+import zmq
 
+#get the  module
 
 def get(request, moduleId):
-    return jsonify({"status": 200})  # Exemple de resposta
+
+    request_json = request.get_json()
+    id = request_json["moduleId"]
+
+
+    # Preparem la connexio amb el worker
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+
+    try:
+        # Ens connectem al worker
+        socket.connect("ipc://backend.ipc")  # TODO: No sabem la adreça de connexio
+
+        try:
+            # Enviem el missatge
+            socket.send_json({"id": id})
+            try:
+                # Esperem la resposta
+                message = socket.recv()
+                if message == "200":
+                    return jsonify({"status": 200})
+                else:
+                    # TODO: Especificar el error
+                    return jsonify( 
+                        {
+                            "errors": [
+                                {
+                                    "error": "",
+                                    "message": "Get Failed",
+                                    "detail": " ",
+                                }
+                            ],
+                            "code": 400,
+                        }
+                    )
+            except Exception as e:
+                # TODO: Especificar el error
+                return jsonify(
+                    {
+                        "errors": [
+                            {"error": "", "message": str(e), "detail": "Receive Failed"}
+                        ],
+                        "code": 400,
+                    }
+                )
+        except Exception as errorSending:
+            # TODO: Especificar el error
+            return jsonify(
+                {
+                    "errors": [
+                        {"error": "", "message": str(errorSending), "detail": "Send Failed"}
+                    ],
+                    "code": 400,
+                }
+            )
+
+    except Exception as errorConnecting:
+        # TODO: Especificar el error
+        return jsonify(
+            {
+                "errors": [
+                    {"error": "", "message": str(errorConnecting), "detail": "Connection Failed"}
+                ],
+                "code": 400,
+            }
+        )
