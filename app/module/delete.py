af634ac9145a088e5cdced303ebca9bfc77e95d9
ventura <itri2805@gmail.com>
2023-03-29 11:31:17 +0200

    Delete maker

--- app/module/delete.py
+++ app/module/delete.py
@@ -1,5 +1,30 @@
 from flask import jsonify
-
+import zmq
 
 def delete(request, moduleId):
-    return jsonify({"status": 200})  # Exemple de resposta
+
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+
+    try:
+        socket.connect("ipc://backend.ipc")
+
+        try:
+            socket.send_string(f'DELETE;MODULE;{moduleId}')
+
+            try:
+                message = socket.recv()
+
+                if message == '200':
+                    return jsonify({"status": 200})
+                else:
+                    return jsonify({"errors": [{"error": "", "message": "", "detail": ""}], "code": 400})
+                
+            except Exception as e:
+                return jsonify({"errors": [{"error": "", "message": str(e), "detail": ""}], "code": 400})
+            
+        except Exception as e:
+            return jsonify({"errors": [{"error": "", "message": str(e), "detail": ""}], "code": 400})
+        
+    except Exception as e:
+        return jsonify({"errors": [{"error": "", "message": str(e), "detail": ""}], "code": 400})
