d6bad3ade1217bfebcf964aa2b22d6aa53a02d79
Biel Camprubí <bielcamprubi@gmail.com>
2023-04-01 18:38:52 +0200

    Improved update.py

--- app/module/update.py
+++ app/module/update.py
@@ -5,32 +5,24 @@ import zmq
 def update(request, moduleId):
     data = request.get_json()
 
-    name = data['name']
-    type = data['type']
-    module = data['module']
+    name = data["name"]
+    type = data["type"]
+    module = data["module"]
 
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
 
     try:
         socket.connect("ipc://backend.ipc")
+        socket.send_string(f"{name};{type};{module};PUT;MODULE;{moduleId}")
+        message = socket.recv()
 
-        try:
-            socket.send_string(f'{name};{type};{module};PUT;MODULE;{moduleId}')
+        if message == "200":
+            return jsonify({"status": 200})
+        else:
+            raise Exception("")
 
-            try:
-                message = socket.recv()
-
-                if message == '200':
-                    return jsonify({"status": 200})
-                else:
-                    return jsonify({"errors": [{"error": "", "message": "", "detail": ""}], "code": 400})
-                
-            except Exception as e:
-                return jsonify({"errors": [{"error": "", "message": str(e), "detail": ""}], "code": 400})
-            
-        except Exception as e:
-            return jsonify({"errors": [{"error": "", "message": str(e), "detail": ""}], "code": 400})
-        
     except Exception as e:
-        return jsonify({"errors": [{"error": "", "message": str(e), "detail": ""}], "code": 400})
+        return jsonify(
+            {"errors": [{"error": "", "message": str(e), "detail": ""}], "code": 400}
+        )
