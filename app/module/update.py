d73effce69f7f21da9063e81921fa268072355ab
mjordanaam <mjordana17@gmail.com>
2023-04-02 17:10:02 +0200

    PUT modificat

--- app/module/update.py
+++ app/module/update.py
@@ -1,27 +1,22 @@
 from flask import jsonify
+from helpers.controller import controller
 import zmq
 
 
 def update(request, name):
     data = request.get_json()
 
-    name = data["name"]
     type = data["type"]
     module = data["module"]
 
-    context = zmq.Context()
-    socket = context.socket(zmq.REQ)
+    singleton = controller()
 
     try:
-        socket.connect("ipc://backend.ipc")
-        socket.send_string(f"{name};{type};{module};PUT;MODULE;{name}")
-        message = socket.recv()
-
-        if message == "200":
-            return jsonify({"status": 200})
-        else:
-            raise Exception("")
-
+        for message in singleton.send_message(f"PUT;MODULE;{name};{type};{module}"):
+            if message != "200":
+                raise Exception("")
+            
+        return jsonify({"status": 200})
     except Exception as e:
         return jsonify(
             {"errors": [{"error": "", "message": str(e), "detail": ""}], "code": 400}
