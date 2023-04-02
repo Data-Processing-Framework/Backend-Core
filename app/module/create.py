a21b6fec1719da7cabc85ac5e6e1da850fd6ddd5
Adaliii <adaliavictor@gmail.com>
2023-04-02 17:09:44 +0200

    POST arreglat

--- app/module/create.py
+++ app/module/create.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+from helpers.controller import controller
 import zmq
 
 
@@ -6,67 +7,17 @@ def create(request):
 
     request_json = request.get_json()
     name = request_json["name"]
-    id = request_json["id"]
     type = request_json["type"]
     module = request_json["module"]
 
     # Preparem la connexio amb el worker
-    context = zmq.Context()
-    socket = context.socket(zmq.REQ)
+    singleton = controller()
     try:
-        # Ens connectem al worker
-        socket.connect("ipc://backend.ipc")  # TODO: No sabem la adreça de connexio
-
-        try:
-            # Enviem el missatge
-            socket.send_json({"Method": "POST", "name": name, "id": id, "type": type, "module": module})
-            try:
-                # Esperem la resposta
-                message = socket.recv()
-                if message == "200":
-                    return jsonify({"status": 200})
-                else:
-                    # TODO: Especificar el error
-                    return jsonify( 
-                        {
-                            "errors": [
-                                {
-                                    "error": "",
-                                    "message": "Creation Failed",
-                                    "detail": "Module already exists",
-                                }
-                            ],
-                            "code": 400,
-                        }
-                    )
-            except Exception as e:
-                # TODO: Especificar el error
-                return jsonify(
-                    {
-                        "errors": [
-                            {"error": "", "message": str(e), "detail": "Receive Failed"}
-                        ],
-                        "code": 400,
-                    }
-                )
-        except Exception as errorSending:
-            # TODO: Especificar el error
-            return jsonify(
-                {
-                    "errors": [
-                        {"error": "", "message": str(errorSending), "detail": "Send Failed"}
-                    ],
-                    "code": 400,
-                }
-            )
-
-    except Exception as errorConnecting:
-        # TODO: Especificar el error
+        for message in singleton.send_message(f"CREATE;MODULE;{name};{type};{module}"):
+            if message != "200":
+                raise Exception("")
+        return jsonify({"name": name, "id": id, "type": type, "module": module}, 200)
+    except Exception as e:
         return jsonify(
-            {
-                "errors": [
-                    {"error": "", "message": str(errorConnecting), "detail": "Connection Failed"}
-                ],
-                "code": 400,
-            }
+            {"errors": [{"error": "", "message": str(e), "detail": ""}], "code": 400}
         )
