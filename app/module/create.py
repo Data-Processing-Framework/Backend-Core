1db2584f6c8938ad866b542eadb146f2c48dd2c1
Adaliii <adaliavictor@gmail.com>
2023-04-02 17:37:08 +0200

    POST arreglat parte 2

--- app/module/create.py
+++ app/module/create.py
@@ -1,22 +1,19 @@
+import json
 from flask import jsonify
-from helpers.controller import controller
+from app.helpers import controller
+
 import zmq
 
 
 def create(request):
 
     request_json = request.get_json()
-    name = request_json["name"]
-    type = request_json["type"]
-    module = request_json["module"]
-
-    # Preparem la connexio amb el worker
     singleton = controller()
     try:
-        for message in singleton.send_message(f"CREATE;MODULE;{name};{type};{module}"):
+        for message in singleton.send_message(f"CREATE;MODULE;{json.dumps(request_json)}"):
             if message != "200":
                 raise Exception("")
-        return jsonify({"name": name, "id": id, "type": type, "module": module}, 200)
+        return jsonify({"status": 200})
     except Exception as e:
         return jsonify(
             {"errors": [{"error": "", "message": str(e), "detail": ""}], "code": 400}
