28f464cb4b51b2714d881484abe662e82fa662f7
Adaliii <adaliavictor@gmail.com>
2023-04-12 11:46:19 +0200

    GET done

--- app/graph/get.py
+++ app/graph/get.py
@@ -1,5 +1,16 @@
 from flask import jsonify
+import json
+import os
 
 
 def get(request):
-    return jsonify({"status": 200})  # Exemple de resposta
+    try:
+        if "graph.json" not in os.listdir("./app/data/"):
+            raise Exception("File does not exist")
+        else:
+            with open("./app/data/graph.json", "r") as graph_file:
+                graph = json.load(graph_file)
+                return jsonify(graph), 200
+    except Exception as e:
+        return jsonify({"errors": [{"error": "Core error", "message": str(e), "detail": "PGraph does not exist, try adding a node"}], "code": 400}), 400
+    
