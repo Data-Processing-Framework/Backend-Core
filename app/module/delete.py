3136f1e73ed550e58ac63bb642667eac4cdcf40a
mjordanaam <mjordana17@gmail.com>
2023-04-12 12:00:25 +0200

    DELETE fixed

--- app/module/delete.py
+++ app/module/delete.py
@@ -2,33 +2,107 @@ from flask import jsonify
 from app.helpers.controller import controller
 import json
 import os
-import zmq
 
 
-def delete(request, name):
+def delete(requests, name):
     
-    data = request.get_json()
-    name = data["name"]
-    context = zmq.Context()
-    socket = context.socket(zmq.REQ)
-    if "modules.json" not in os.listdir("./app/data/"):
-            raise Exception("File does not exist")
+    singleton = controller()
 
-    if os.path.getsize("./app/data/modules.json") == 0:
-        raise Exception("File Empty")
+    try:
+        if "modules.json" not in os.listdir("./app/data/"):
+                raise Exception("File does not exist")
 
-    with open("./app/data/modules.json", "r") as module_file:
-        modules = json.load(module_file)
-    modules = modules["modules"]
+        if os.path.getsize("./app/data/modules.json") == 0:
+            raise Exception("File Empty")
 
-    for module in modules:
-        if module["name"] == name:
-             module.pop(module)
-        
+        with open("./app/data/modules.json", "r") as module_file:
+            modules = json.load(module_file)
+            found = False
 
-    print(module)
-    with open('./app/data/modules.json', 'w') as module_file:
-        module_file.write('')
-        module_file.close()
-    with open("./app/data/modules.json", "r") as module_file:
-        json.dump(module_file)
+            for module in modules:
+                if module["name"] == name:
+                    modules.remove(module)
+                    found = True
+                    break
+
+            if found:
+                with open('./app/data/modules.json', 'w') as module_file:
+                    json.dump(modules, module_file)
+
+            if name + ".py" in os.listdir("./app/data/modules"):
+                os.remove("./app/data/modules/" + name + ".py")
+            else:
+                raise Exception("Module does not exist")
+
+        message = singleton.send_message("RESTART")
+
+        if message["code"] == 200:
+            return jsonify(message), 200
+        else:
+            return jsonify(message), 400
+    except Exception as e:
+        if str(e) == "File does not exist":
+            return (
+                jsonify(
+                    {
+                        "errors": [
+                            {
+                                "error": "Core error",
+                                "message": str(e),
+                                "detail": "Please check the file name and location and try again.",
+                            }
+                        ],
+                        "code": 400,
+                    }
+                ),
+                400,
+            )
+        elif str(e) == "File Empty":
+            return (
+                jsonify(
+                    {
+                        "errors": [
+                            {
+                                "error": "Core error",
+                                "message": str(e),
+                                "detail": "Please create a module before updating it, and then try again.",
+                            }
+                        ],
+                        "code": 400,
+                    }
+                ),
+                400,
+            )
+        elif str(e) == "Module does not exist":
+            return (
+                jsonify(
+                    {
+                        "errors": [
+                            {
+                                "error": "Core error",
+                                "message": str(e),
+                                "detail": "Please check the module name and try again.",
+                            }
+                        ],
+                        "code": 400,
+                    }
+                ),
+                400,
+            )
+        else:
+            return (
+                jsonify(
+                    {
+                        "errors": [
+                            {
+                                "error": "Core error",
+                                "message": str(e),
+                                "detail": "Please try again.",
+                            }
+                        ],
+                        "code": 400,
+                    }
+                ),
+                400,
+            )
+
