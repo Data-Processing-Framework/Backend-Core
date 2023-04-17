d84b1586bf7fe67ad2662ceb191197824f9e84e4
bonaventura89 <57214868+bonaventura89@users.noreply.github.com>
2023-04-17 10:40:45 +0200

    Update restart.py
    modificar 2 restart

--- app/system/restart.py
+++ app/system/restart.py
@@ -2,5 +2,5 @@ from flask import jsonify
 from app.helpers.controller import controller
 
 def restart(request):
-    Answer = controller.send_message(restart)
+    Answer = controller.send_message("restart")
     return jsonify(Answer)  # Exemple de resposta
