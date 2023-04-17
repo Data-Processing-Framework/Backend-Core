7c48adbd258ee698750eec7f768ff37358dd7769
bonaventura89 <57214868+bonaventura89@users.noreply.github.com>
2023-04-17 11:15:44 +0200

    Update restart.py
    Restart modify3

--- app/system/restart.py
+++ app/system/restart.py
@@ -2,5 +2,23 @@ from flask import jsonify
 from app.helpers.controller import controller
 
 def restart(request):
-    Answer = controller.send_message("restart")
-    return jsonify(Answer)  # Exemple de resposta
+    Controll = controller()
+    try:
+        Answer = Controll.send_message("RESTART")
+        return jsonify(Answer)  # Exemple de resposta
+    except Exception as e:
+        return (
+                jsonify(
+                    {
+                        "errors": [
+                            {
+                                "error": "Type of error",
+                                "message": "Error message",
+                                "detail": "Restart fail"
+                            }
+                        ],
+                        "code": 400
+                    }
+                ),
+                400,
+            )
