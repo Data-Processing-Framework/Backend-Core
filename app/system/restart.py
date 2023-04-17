b730b06bf8610739a20888431b999c8202ccb18d
Biel Camprubí <bielcamprubi@gmail.com>
2023-04-17 11:59:10 +0200

    Small changes

--- app/system/restart.py
+++ app/system/restart.py
@@ -1,31 +1,29 @@
 from flask import jsonify
 from app.helpers.controller import controller
-from app.system.status import status
+
 
 def restart(request):
-    Controll = controller()
-    Estado = status()
     try:
-        if Estado.status() == 200:
-            Answer = Controll.send_message("RESTART")
-            if Answer["code"] == 400:
-                return jsonify(Answer), 400
-            else:
-                return jsonify(Answer), 200
-        
+        Controll = controller()
+        Answer = Controll.send_message("RESTART")
+        if Answer["code"] == 400:
+            return jsonify(Answer), 400
+        else:
+            return jsonify(Answer), 200
+
     except Exception as e:
         return (
-                jsonify(
-                    {
-                        "errors": [
-                            {
-                                "error": "Restart fail",
-                                "message": str(e),
-                                "detail": "stop the computer and open the computer again"
-                            }
-                        ],
-                        "code": 400
-                    }
-                ),
-                400,
-            )
+            jsonify(
+                {
+                    "errors": [
+                        {
+                            "error": "Restart fail",
+                            "message": str(e),
+                            "detail": "Reboot the system",
+                        }
+                    ],
+                    "code": 400,
+                }
+            ),
+            400,
+        )
