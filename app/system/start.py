155caa47c6b11b6095c866facebacf65a4dc245e
Biel Camprubí <bielcamprubi@gmail.com>
2023-04-17 11:52:32 +0200

    Small changes

--- app/system/start.py
+++ app/system/start.py
@@ -1,27 +1,25 @@
 from flask import jsonify
 from app.helpers.controller import controller
 
-def status(): # funcion inventada, luego sera status.py
+
+def status():  # funcion inventada, luego sera status.py
     status = 200
     return status
 
+
 def start(request):
-    
 
     try:
-        if (status() == 200):
-            raise Exception("Server is already on.")
+        controlador = controller()
+        missatge = str("START")
+        resposta = controlador.send_message(missatge)
+
+        if resposta["code"] == 200:
+            print("start")
+            return jsonify({"status": 200})
         else:
-            controlador = controller()
-            missatge = str("START")
-            resposta = controlador.send_message(missatge)
-            
-            if(resposta == 200):
-                print("start")
-                return jsonify({"status": 200})
-            else:
-                raise Exception("Server couldn't be started.")
-            
+            return resposta, 400
+
     except Exception as e:
         if str(e) == "Server is already on.":
             return (
@@ -31,7 +29,7 @@ def start(request):
                             {
                                 "error": "Server already on",
                                 "message": str(e),
-                                "detail": "Server is already on, START only works when server ain't already running"
+                                "detail": "Server is already on, START only works when server ain't already running",
                             }
                         ],
                         "code": 400,
@@ -47,7 +45,7 @@ def start(request):
                             {
                                 "error": "Server couldn't start",
                                 "message": str(e),
-                                "detail": "Server couldn't get started"
+                                "detail": "Server couldn't get started",
                             }
                         ],
                         "code": 400,
