018cdfe7c659a3b5185b39de5b1e130f4b2a34fd
bonaventura89 <57214868+bonaventura89@users.noreply.github.com>
2023-04-17 11:49:40 +0200

    Update restart.py
    Status añadido

--- app/system/restart.py
+++ app/system/restart.py
@@ -1,14 +1,17 @@
 from flask import jsonify
 from app.helpers.controller import controller
+from app.system.status import status
 
 def restart(request):
     Controll = controller()
+    Estado = status()
     try:
-        Answer = Controll.send_message("RESTART")
-        if Answer["code"] == 400:
-            return jsonify(Answer), 400
-        else:
-            return jsonify(Answer), 200
+        if Estado.status() == 200:
+            Answer = Controll.send_message("RESTART")
+            if Answer["code"] == 400:
+                return jsonify(Answer), 400
+            else:
+                return jsonify(Answer), 200
         
     except Exception as e:
         return (
