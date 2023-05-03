6a454d3bb505757225d6b682a34e779371f9873e
hcapilla <80215689+hcapilla@users.noreply.github.com>
2023-05-03 09:34:58 +0200

    start.py working

--- app/system/start.py
+++ app/system/start.py
@@ -1,55 +1,30 @@
 from flask import jsonify
 from app.helpers.controller import controller
 
-
-def status():  # funcion inventada, luego sera status.py
-    status = 200
-    return status
-
-
 def start(request):
-
+    
     try:
         controlador = controller()
-        missatge = str("START")
-        resposta = controlador.send_message(missatge)
+        resposta = controlador.send_message("START")
 
         if resposta["code"] == 200:
-            print("start")
-            return jsonify({"status": 200})
+            return jsonify(resposta), 200
         else:
-            return resposta, 400
-
+            return jsonify(resposta), 400
+        
     except Exception as e:
-        if str(e) == "Server is already on.":
-            return (
-                jsonify(
-                    {
-                        "errors": [
-                            {
-                                "error": "Server already on",
-                                "message": str(e),
-                                "detail": "Server is already on, START only works when server ain't already running",
-                            }
-                        ],
-                        "code": 400,
-                    }
-                ),
-                400,
-            )
-        elif str(e) == "Server couldn't be started.":
-            return (
-                jsonify(
-                    {
-                        "errors": [
-                            {
-                                "error": "Server couldn't start",
-                                "message": str(e),
-                                "detail": "Server couldn't get started",
-                            }
-                        ],
-                        "code": 400,
-                    }
-                ),
-                400,
-            )
+        return (
+            jsonify(
+                {
+                    "errors": [
+                        {
+                            "error": "Server already on",
+                            "message": str(e),
+                            "detail": "Server is already on, START only works when server ain't already running"
+                        }
+                    ],
+                    "code": 400,
+                }
+            ),
+            400,
+        )
