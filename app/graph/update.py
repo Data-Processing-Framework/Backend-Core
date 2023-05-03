e05f5cfa444f164a9aa659ae8b05681e0aa6c728
bonaventura89 <57214868+bonaventura89@users.noreply.github.com>
2023-05-03 11:57:01 +0200

    Update update.py

--- app/graph/update.py
+++ app/graph/update.py
@@ -2,12 +2,29 @@ from app.helpers.controller import controller
 from flask import jsonify
 import json
 import os
+required_fields = ["name", "type", "module", "inputs", "postion"]
 
+class MissingFieldException(Exception):
+    pass
+
+def validate_json(json_data):
+    try:
+        data = json.loads(json_data)
+    except ValueError:
+        raise MissingFieldException("Al JSON le faltan campos")
+
+    for field in required_fields:
+        if field not in data or not data[field]:
+            raise MissingFieldException(f"Falta el campo '{field}' en el JSON")
+
+    return True
 
 def update(request):
     try:
         data = request.get_json()
         singleton = controller()
+        
+        validate_json(data)
 
         if "graph.json" not in os.listdir("./app/data/"):
             raise Exception("File does not exist")
@@ -24,6 +41,24 @@ def update(request):
             return jsonify(message), 200
         else:
             return jsonify(message), 400
+    
+    except MissingFieldException as e:
+        if str(e) == f"The field '{e}' is missing from the JSON":
+            return (
+                jsonify(
+                    {
+                        "errors": [
+                            {
+                                "error": "Error Archive",
+                                "message": str(e),
+                                "detail": "Restart system",
+                            }
+                        ],
+                        "code": 400,
+                    }
+                ),
+                400,
+            )
 
     except Exception as e:
         if str(e) == "File does not exist":
