369e2fd5449244c0205b9a2d822f46e51fff3904
Biel Camprubí <bielcamprubi@gmail.com>
2023-05-16 16:25:20 +0200

    Fixed small bug

--- app/graph/routes.py
+++ app/graph/routes.py
@@ -3,16 +3,16 @@ from flask import request
 from app.graph import bp, create, get, update
 
 
-@bp.route("/", methods=["POST"])
+@bp.route("", methods=["POST"])
 def create_route():
     return create.create(request)
 
 
-@bp.route("/", methods=["GET"])
+@bp.route("", methods=["GET"])
 def get_route():
     return get.get(request)
 
 
-@bp.route("/", methods=["PUT"])
+@bp.route("", methods=["PUT"])
 def update_route():
     return update.update(request)
