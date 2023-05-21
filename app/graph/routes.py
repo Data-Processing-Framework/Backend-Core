69a3e62b3556f7bc77b73cb9c330437203306222
AlSaBib98 <92037047+AlSaBib98@users.noreply.github.com>
2023-05-21 12:20:02 +0200

    Update routes.py

--- app/graph/routes.py
+++ app/graph/routes.py
@@ -3,16 +3,15 @@ from flask import request
 from app.graph import bp, create, get, update
 
 
-@bp.route("", methods=["POST"])
-def create_route():
-    return create.create(request)
+@bp.route("/<name>", methods=["POST"])
+def create_route(name):
+    return create.create(request, name)
 
-
-@bp.route("", methods=["GET"])
+@bp.route("/", methods=["GET"])
 def get_route():
     return get.get(request)
 
 
-@bp.route("", methods=["PUT"])
+@bp.route("/", methods=["PUT"])
 def update_route():
     return update.update(request)
